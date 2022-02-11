e5db6ac08f889e2879712634d742fcf290ef5243
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-02-11 10:10:56 -0300

    :sparkles: Authentication (#2)
    * :construction: Sign in button auth working
    
    * :construction: User data saved on database
    
    Setup faunadb to create a document that contains users data

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import { query as q } from 'faunadb';
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user"
+        }
+      }
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            q.Create(q.Collection('users'), { data: { email } }),
+          )
+        )
+  
+        return true;
+      } catch (error) {
+        console.error(error);
+        
+        return false;
+      }
+    }
+  }
+})
